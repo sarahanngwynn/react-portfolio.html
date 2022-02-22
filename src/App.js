26b32704bf889490ea389ea9289029a151c6ba6d
sarahanngwynn <93155807+sarahanngwynn@users.noreply.github.com>
2022-02-21 17:04:41 -0700

    added pages, navbar and some css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
 
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
+      <div className="container">
+        <Navbar />
+      </div>
+      <div className="hero">
+        <p>👋 Hello, I'm </p>
+        <h1>Sarah Gwynn</h1>
+        <h2 className="mb-5">Front-end developer based in the United States</h2>
+        <div>
+          <a
+            href="./contact.html"
+            className="btn btn-branding"
+            title="Contact me"
+          >
+            Contact me
+          </a>
+        </div>
+      </div>
+      <p className="text-center m-5">
+        Check out some of my feature project below
+      </p>
+      <Projects />
     </div>
   );
 }
---
+++ src/Projects.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div className="container">
+      <div className="row mb-5">
+        <div className="col d-none d-lg-block">
+          <img
+            src="images/yogurt.png"
+            className="img-fluid"
+            alt="Yogurt project preview"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
